c505a460d9daec198623d1c5b95f9335f3004db1
eleday <leotol1432@gmail.com>
2024-11-14 07:29:12 +0300

    do test, result (unfinished)

--- databaser.py
+++ databaser.py
@@ -1,11 +1,12 @@
 import sqlite3
+import csv
 
 
 class Databaser:
 
     def __init__(self, filename='tests.db'):
 
-        self.connection = sqlite3.connect(filename)
+        self.connection = sqlite3.connect(filename, check_same_thread=False)
         self.cursor = self.connection.cursor()
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS tests (
             id INTEGER PRIMARY KEY,
@@ -38,15 +39,17 @@ class Databaser:
 
     def get_test_id(self, name):
         self.cursor.execute('''SELECT id FROM tests WHERE name = ?''', (name,))
-        return self.cursor.fetchone()[0]
+        r = self.cursor.fetchone()
+
+        return r[0] if r is not None else None
 
     def get_questions(self, test_id):
         self.cursor.execute('''SELECT value FROM questions WHERE test_id = ?''', (test_id,))
-        return self.cursor.fetchall()
+        return list(map(lambda x: x[0], self.cursor.fetchall()))
 
     def get_answers(self, test_id):
         self.cursor.execute('''SELECT value FROM answers WHERE test_id = ?''', (test_id,))
-        return self.cursor.fetchall()
+        return list(map(lambda x: x[0], self.cursor.fetchall()))
 
     def get_test_names(self):
         self.cursor.execute('''SELECT name FROM tests''')
@@ -62,3 +65,25 @@ class Databaser:
             self.add_answer(test_id, a)
 
         self.connection.commit()
+
+
+def clear_str(s):
+    return s.replace('"', '\'').replace(';', ',').lower().replace('ё', 'е').strip()
+
+
+class Filer:
+
+    def save_answers(self, answers, db, test_id, filename='answers.csv'):
+        answers = [clear_str(e) for e in answers]
+        right_answers = [clear_str(e) for e in db.get_answers(test_id)]
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            writer = csv.writer(file, delimiter=';', quotechar='"')
+
+            for i, (a, r) in enumerate(zip(answers, right_answers)):
+                writer.writerow([i, a, r == a])
+
+
+if __name__ == '__main__':
+    f = Filer()
+    f.save_answers(['absd;"fg1', 'a42'])
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QInputDialog, QHeaderView, QErrorMessage
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QInputDialog, QHeaderView, QErrorMessage, QRadioButton, QLabel, QLineEdit, QHBoxLayout, QPushButton
+from PyQt6.QtGui import QPixmap
 from PyQt6 import uic
 
-from databaser import Databaser
+from PIL import Image, ImageDraw, ImageFont
+
+from databaser import Databaser, Filer
 
 import sys
 
 
 db = Databaser()
+filer = Filer()
 
 
 class MainWindow(QMainWindow):
@@ -15,10 +19,10 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         uic.loadUi('main.ui', self)
-
         self.setWindowTitle('Тестовая система')
-
         self.initUI()
+
+        self.second_window = None
 
     def initUI(self):
         self.create_btn.clicked.connect(self.create_test)
@@ -29,7 +33,8 @@ class MainWindow(QMainWindow):
         self.export_btn.clicked.connect(self.export_test)
 
     def do_test(self):
-        print('Прохождение теста')
+        self.second_window = ChoiceTestWindow()
+        self.second_window.show()
 
     def create_test(self):
         self.second_window = CreateWindow()
@@ -69,6 +74,8 @@ class CreateWindow(QMainWindow):
         header.setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
 
     def add_question(self):
+        # TODO Добавить вопросы с вариантами ответов
+
         q, ok_pressed = QInputDialog.getText(self, 'Создание вопроса', 'Введите вопрос')
         if not ok_pressed:
             return
@@ -91,6 +98,113 @@ class CreateWindow(QMainWindow):
             self.error.showMessage('Название теста не может быть пустым')
         elif not self.qs:
             self.error.showMessage('Тест должен содержать хотя бы один вопрос')
+
+
+class ChoiceTestWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('choice_test.ui', self)
+        self.setWindowTitle('Выбор теста')
+        self.initUI()
+
+    def initUI(self):
+        tests = db.get_test_names()
+
+        self.test_btns = []
+        for i, test in enumerate(tests):
+            self.test_btns.append(QRadioButton(test[0], parent=self.groupBox))
+            self.test_btns[-1].move(25, i * 25 + 25)
+            self.test_btns[-1].toggled.connect(self.select_test)
+
+        self.start_btn.setEnabled(False)
+        self.start_btn.clicked.connect(self.start_test)
+
+    def select_test(self):
+        self.start_btn.setEnabled(True)
+
+    def start_test(self):
+        test_name = list(filter(lambda x: x.toggled, self.test_btns))[0].text()
+        test_id = db.get_test_id(test_name)
+        self.second_window = TestWindow(test_id)
+        self.second_window.show()
+        self.close()
+
+
+class TestWindow(QMainWindow):
+
+    def __init__(self, test_id):
+
+        self.test_id = test_id
+        self.test_name = db.get_test_name(self.test_id)
+        self.qs = db.get_questions(self.test_id)
+
+        self.question_labels = []
+        self.question_line_edits = []
+
+        super().__init__()
+
+        uic.loadUi('test.ui', self)
+        self.setWindowTitle(f'Тест "{self.test_name}"')
+        self.initUI()
+
+    def initUI(self):
+        for i, q in enumerate(self.qs):
+            self.question_labels.append(QLabel(f'{i + 1}. {q}', self))
+            self.question_line_edits.append(QLineEdit(self))
+
+            self.question_labels[-1].setContentsMargins(0, 0, 15, 0)
+
+            hb = QHBoxLayout(self)
+            hb.addWidget(self.question_labels[-1])
+            hb.addWidget(self.question_line_edits[-1])
+
+            hb.setContentsMargins(0, 15, 0, 0)
+
+            self.verticalLayout.addLayout(hb)
+
+        self.finish_btn.clicked.connect(self.finish_test)
+
+    def finish_test(self):
+        answers = [x.text() for x in self.question_line_edits]
+        filer.save_answers(answers, db, self.test_id)
+
+        self.close()
+        self.second_window = ResultWindow()
+        self.second_window.show()
+
+
+class ResultWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('result.ui', self)
+
+        self.setWindowTitle('Результат теста')
+        self.setMaximumSize(500, 500)
+        self.setMinimumSize(500, 500)
+
+        self.initUI()
+
+    def initUI(self):
+        make_result_image()
+
+        self.pixmap = QPixmap('result.png')
+
+        self.img_lbl.setPixmap(self.pixmap)
+
+
+def make_result_image(right_count, all_count):
+    img = Image.new('RGBA', (500, 500), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    myFont = ImageFont.truetype('Roboto.ttf', 20)
+
+    draw.text((200, 150), 'Результат теста', font=myFont, fill=(255, 255, 255, 255))
+
+    draw.rectangle((200, 150, 300, 350), fill=(255, 255, 255, 255))
+    img.save('result.png')
 
 
 if __name__ == '__main__':
